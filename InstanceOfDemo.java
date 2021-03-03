6e0426f1253c512f17447288bcdc46b3dca3afd8
632115022thannavat <thannavat_v@elearning.cmu.ac.th>
2021-03-03 12:12:38 +0700

    week11

---
+++ InstanceOfDemo.java
@@ -0,0 +1,18 @@
+
+class A{
+
+}
+class B extends A{
+
+}
+class C extends B{
+
+}
+public class InstanceOfDemo{
+ public static void main(String[] args) {
+     A a = new A();
+     B b = new B();
+     Object c = new C();
+     System.out.println(b instanceof C);
+ }
+}
